ec9fc00c3e24886d63a94968b54b9a9fd37e38bd
jeet7122 <jeetthakkar2612@gmail.com>
2025-08-19 02:06:23 -0400

    finding position of an element using binary search on infinite array

---
+++ src/main/java/InfiniteArray.java
@@ -0,0 +1,34 @@
+public class InfiniteArray {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 15, 18, 25, 25, 35, 45};
+        int target = 7;
+        int pos = searchPos(nums, target);
+        System.out.println(pos);
+    }
+
+    static int searchPos(int[] nums, int target) {
+        int start = 0;
+        int end = 1;
+        while (target > nums[end]) {
+            int temp = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = temp;
+        }
+        return binarySearch(nums, target, start, end);
+    }
+
+    private static int binarySearch(int[] nums, int target, int start, int end) {
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < target) {
+                end = mid - 1;
+            } else if (nums[mid] > target) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
